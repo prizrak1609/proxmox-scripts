736cce8be5f2ecff07cbd8603081530c84a11bd5
Dima Hubatenko <gubatenkod@gmail.com>
2025-06-29 22:59:53 +0300

    added actual

---
+++ actual-budget/actual-install.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/bash
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y make git
+msg_ok "Installed Dependencies"
+
+NODE_VERSION="22"
+setup_nodejs
+
+msg_info "Installing Mono-to-Actual"
+$STD mkdir -p /home/mono-to-actual
+$STD git clone https://github.com/prizrak1609/mono-to-actualbudget.git /home/mono-to-actual/
+$STD cd /home/mono-to-actual
+$STD npm install
+msg_ok "Installed Mono-to-Actual"
+
+msg_info "Installing Actual Budget"
+cd /opt
+RELEASE=$(curl -fsSL https://api.github.com/repos/actualbudget/actual/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+mkdir -p /opt/actualbudget-data/{server-files,upload,migrate,user-files,migrations,config}
+chown -R root:root /opt/actualbudget-data
+chmod -R 755 /opt/actualbudget-data
+
+cat <<EOF >/opt/actualbudget-data/config.json
+{
+  "port": 5006,
+  "hostname": "0.0.0.0",
+  "serverFiles": "/opt/actualbudget-data/server-files",
+  "userFiles": "/opt/actualbudget-data/user-files",
+  "trustedProxies": [
+    "10.0.0.0/8",
+    "172.16.0.0/12",
+    "192.168.0.0/16",
+    "127.0.0.0/8",
+    "::1/128",
+    "fc00::/7"
+  ],
+  "https": {
+    "key": "/opt/actualbudget/selfhost.key",
+    "cert": "/opt/actualbudget/selfhost.crt"
+  }
+}
+EOF
+
+mkdir -p /opt/actualbudget
+cd /opt/actualbudget
+$STD npm install --location=global @actual-app/sync-server
+$STD openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout selfhost.key -out selfhost.crt <<EOF
+US
+California
+San Francisco
+My Organization
+My Unit
+localhost
+myemail@example.com
+EOF
+echo "${RELEASE}" >"/opt/actualbudget_version.txt"
+msg_ok "Installed Actual Budget"
+
+msg_info "Creating Service"
+cat <<EOF >/etc/systemd/system/actualbudget.service
+[Unit]
+Description=Actual Budget Service
+After=network.target
+
+[Service]
+Type=simple
+User=root
+Group=root
+WorkingDirectory=/opt/actualbudget
+Environment=ACTUAL_UPLOAD_FILE_SIZE_LIMIT_MB=20
+Environment=ACTUAL_UPLOAD_SYNC_ENCRYPTED_FILE_SYNC_SIZE_LIMIT_MB=50
+Environment=ACTUAL_UPLOAD_FILE_SYNC_SIZE_LIMIT_MB=20
+ExecStart=/usr/bin/actual-server --config /opt/actualbudget-data/config.json
+Restart=always
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable -q --now actualbudget
+msg_ok "Created Service"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
---
+++ actual-budget/actual.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+tmp_dir=$(mktemp -d)
+
+curl -L https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func -o "$tmp_dir/build.func"
+
+# remove
+# lxc-attach -n "$CTID" -- bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/install/"$var_install".sh)" $?
+# from script so it is usable now
+sed -i '/lxc-attach -n "$CTID" -- bash -c "$(curl -fsSL https:\/\/raw.githubusercontent.com\/community-scripts\/ProxmoxVE\/main\/install\/"$var_install".sh)" $?/d' "$tmp_dir/build.func"
+
+source <(cat "$tmp_dir/build.func")
+
+APP="Actual Budget"
+var_tags="finance"
+var_cpu="1"
+var_ram="1024"
+var_disk="1"
+var_os="debian"
+var_version="12"
+var_unprivileged="0"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+
+  if [[ ! -f /opt/actualbudget_version.txt ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  NODE_VERSION="22"
+  setup_nodejs
+  RELEASE=$(curl -fsSL https://api.github.com/repos/actualbudget/actual/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+  if [[ -f /opt/actualbudget-data/config.json ]]; then
+    if [[ ! -f /opt/actualbudget_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/actualbudget_version.txt)" ]]; then
+      msg_info "Stopping ${APP}"
+      systemctl stop actualbudget
+      msg_ok "${APP} Stopped"
+
+	  msg_info "Updating Mono-to-Actual"
+      $STD cd /home/mono-to-actual
+	  $STD git fetch origin main
+	  $STD git reset --hard origin/main
+      msg_ok "Updated Mono-to-Actual"
+
+      msg_info "Updating ${APP} to ${RELEASE}"
+      $STD npm update -g @actual-app/sync-server
+      echo "${RELEASE}" >/opt/actualbudget_version.txt
+      msg_ok "Updated ${APP} to ${RELEASE}"
+
+      msg_info "Starting ${APP}"
+      systemctl start actualbudget
+      msg_ok "Restarted ${APP}"
+    else
+      msg_info "${APP} is already up to date"
+    fi
+  else
+    msg_info "Old Installation Found, you need to migrate your data and recreate to a new container"
+    msg_info "Please follow the instructions on the ${APP} website to migrate your data"
+    msg_info "https://actualbudget.org/docs/backup-restore/backup"
+    exit 1
+  fi
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}https://${IP}:5006${CL}"
