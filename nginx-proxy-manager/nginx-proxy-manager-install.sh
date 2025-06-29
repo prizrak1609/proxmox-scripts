fbbd2fad09017e61e98d4cbafa8734ee01e34677
Dima Hubatenko <gubatenkod@gmail.com>
2025-06-29 19:00:06 +0300

    moved files

---
+++ nginx-proxy-manager/nginx-proxy-manager-install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+export FUNCTIONS_FILE_PATH="$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/alpine-install.func)"
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apk add docker docker-compose --no-cache
+msg_ok "Installed Dependencies"
+
+msg_info "Configuring docker"
+$STD rc-update add docker
+$STD service docker start
+msg_ok "Configured docker"
+
+msg_info "Configuring nginx-proxy-manager"
+$STD mkdir data
+$STD mkdir letsencrypt
+$STD curl -L https://raw.githubusercontent.com/prizrak1609/proxmox-scripts/refs/heads/main/nginx-proxy-manager-docker-compose.yaml -o docker-compose.yaml
+msg_ok "Configured nginx-proxy-manager"
+
+msg_info "Starting nginx-proxy-manager"
+$STD docker-compose up -d
+msg_ok "Started nginx-proxy-manager"
--- plex/plex-install.sh
+++ plex/plex-install.sh
@@ -11,20 +11,22 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apk add docker docker-compose --no-cache
+$STD echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
+$STD curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
+$STD apt-get update
 msg_ok "Installed Dependencies"
 
 msg_info "Configuring docker"
-rc-update add docker
-service docker start
+$STD rc-update add docker
+$STD service docker start
 msg_ok "Configured docker"
 
 msg_info "Configuring nginx-proxy-manager"
-mkdir data
-mkdir letsencrypt
-curl -L https://raw.githubusercontent.com/prizrak1609/proxmox-scripts/refs/heads/main/nginx-proxy-manager-docker-compose.yaml -o docker-compose.yaml
+$STD mkdir data
+$STD mkdir letsencrypt
+$STD curl -L https://raw.githubusercontent.com/prizrak1609/proxmox-scripts/refs/heads/main/nginx-proxy-manager-docker-compose.yaml -o docker-compose.yaml
 msg_ok "Configured nginx-proxy-manager"
 
 msg_info "Starting nginx-proxy-manager"
-docker-compose up -d
+$STD docker-compose up -d
 msg_ok "Started nginx-proxy-manager"
