2ff8ed201011ccc6b4a1b277e2aab86ddfe2cc2f
Dima Hubatenko <gubatenkod@gmail.com>
2025-06-30 13:30:12 +0300

    added glance

---
+++ glance/glance-install.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get update
+$STD apt-get upgrade -y
+$STD apt-get install -y curl
+msg_ok "Installed Dependencies"
+
+msg_info "Installing Glance"
+RELEASE=$(curl -fsSL https://api.github.com/repos/glanceapp/glance/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+cd /opt
+curl -fsSL "https://github.com/glanceapp/glance/releases/download/v${RELEASE}/glance-linux-amd64.tar.gz" -o "glance-linux-amd64.tar.gz"
+mkdir -p /opt/glance
+tar -xzf glance-linux-amd64.tar.gz -C /opt/glance
+
+curl -L https://raw.githubusercontent.com/prizrak1609/proxmox-scripts/refs/heads/main/glance/glance-config.sh -o /opt/glance/glance.yml
+
+echo "${RELEASE}" >"/opt/${APPLICATION}_version.txt"
+msg_ok "Installed Glance"
+
+msg_info "Creating Service"
+service_path="/etc/systemd/system/glance.service"
+echo "[Unit]
+Description=Glance Daemon
+After=network.target
+
+[Service]
+Type=simple
+WorkingDirectory=/opt/glance
+ExecStart=/opt/glance/glance --config /opt/glance/glance.yml
+TimeoutStopSec=20
+KillMode=process
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target" >$service_path
+
+systemctl enable -q --now glance
+msg_ok "Created Service"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+rm -rf /opt/glance-linux-amd64.tar.gz
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
---
+++ glance/glance.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+tmp_dir=$(mktemp -d)
+
+curl -L https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func -o "$tmp_dir/build.func"
+
+# remove
+# lxc-attach -n "$CTID" -- bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/install/"$var_install".sh)" $?
+# from script so it is usable now
+sed -i '/lxc-attach -n "$CTID" -- bash -c "$(curl -fsSL https:\/\/raw.githubusercontent.com\/community-scripts\/ProxmoxVE\/main\/install\/"$var_install".sh)" $?/d' "$tmp_dir/build.func"
+
+source <(cat "$tmp_dir/build.func")
+
+APP="Glance"
+var_tags="homepage"
+var_cpu="1"
+var_ram="256"
+var_disk="0.5"
+var_os="debian"
+var_version="12"
+var_unprivileged="0"
+var_verbose="1"
+var_fuse="0"
+var_tun="0"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+
+  if [[ ! -f /etc/systemd/system/glance.service ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+
+  RELEASE=$(curl -fsSL https://api.github.com/repos/glanceapp/glance/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+  if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]]; then
+    msg_info "Stopping Service"
+    systemctl stop glance
+    msg_ok "Stopped Service"
+
+    msg_info "Updating ${APP} to v${RELEASE}"
+    cd /opt
+    curl -fsSL "https://github.com/glanceapp/glance/releases/download/v${RELEASE}/glance-linux-amd64.tar.gz" -o $(basename "https://github.com/glanceapp/glance/releases/download/v${RELEASE}/glance-linux-amd64.tar.gz")
+    rm -rf /opt/glance/glance
+    tar -xzf glance-linux-amd64.tar.gz -C /opt/glance
+    echo "${RELEASE}" >"/opt/${APP}_version.txt"
+    msg_ok "Updated ${APP} to v${RELEASE}"
+
+    msg_info "Starting Service"
+    systemctl start glance
+    msg_ok "Started Service"
+
+    msg_info "Cleaning up"
+    rm -rf /opt/glance-linux-amd64.tar.gz
+    msg_ok "Cleaned"
+    msg_ok "Updated Successfully"
+  else
+    msg_ok "No update required. ${APP} is already at v${RELEASE}."
+  fi
+  exit
+}
+
+start
+build_container
+
+lxc-attach -n "$CTID" -- bash -c "$(curl -fsSL https://raw.githubusercontent.com/prizrak1609/proxmox-scripts/refs/heads/main/glance/glance-install.sh)"
+
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8080${CL}"
