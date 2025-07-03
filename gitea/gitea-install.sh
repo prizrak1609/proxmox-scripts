c4556a1f38ecf84a8132668a45f010814d207005
Dima Hubatenko <gubatenkod@gmail.com>
2025-07-03 20:55:51 +0300

    added gitea

---
+++ gitea/gitea-install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Gitea"
+$STD apk add --no-cache gitea
+msg_ok "Installed Gitea"
+
+msg_info "Enabling Gitea Service"
+$STD rc-update add gitea default
+msg_ok "Enabled Gitea Service"
+
+msg_info "Starting Gitea"
+$STD service gitea start
+msg_ok "Started Gitea"
+
+motd_ssh
+customize
---
+++ gitea/gitea.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+tmp_dir=$(mktemp -d)
+
+curl -L https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func -o "$tmp_dir/build.func"
+
+# remove
+# lxc-attach -n "$CTID" -- bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/install/"$var_install".sh)" $?
+# from script so it is usable now
+sed -i '/lxc-attach -n "$CTID" -- bash -c "$(curl -fsSL https:\/\/raw.githubusercontent.com\/community-scripts\/ProxmoxVE\/main\/install\/"$var_install".sh)" $?/d' "$tmp_dir/build.func"
+
+source <(cat "$tmp_dir/build.func")
+
+APP="Gitea"
+var_tags="git"
+var_cpu="1"
+var_ram="256"
+var_disk="1"
+var_os="alpine"
+var_version="3.22"
+var_unprivileged="0"
+var_verbose="1"
+var_fuse="0"
+var_tun="0"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  msg_info "Updating Alpine Packages"
+  $STD apk -U upgrade
+  msg_ok "Updated Alpine Packages"
+
+  msg_info "Updating Gitea"
+  $STD apk upgrade gitea
+  msg_ok "Updated Gitea"
+
+  msg_info "Restarting Gitea"
+  $STD rc-service gitea restart
+  msg_ok "Restarted Gitea"
+}
+
+start
+build_container
+
+lxc-attach -n "$CTID" -- bash -c "$(curl -fsSL https://raw.githubusercontent.com/prizrak1609/proxmox-scripts/refs/heads/main/gitea/gitea-install.sh)"
+
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:3000${CL}"
