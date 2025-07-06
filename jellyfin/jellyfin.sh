dbc4892ec0b28a8352f7e2ec810f057c8d111e72
Dima Hubatenko <gubatenkod@gmail.com>
2025-07-06 13:02:53 +0300

    added jellyfin

---
+++ jellyfin/jellyfin.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+tmp_dir=$(mktemp -d)
+
+curl -L https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func -o "$tmp_dir/build.func"
+
+# remove
+# lxc-attach -n "$CTID" -- bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/install/"$var_install".sh)" $?
+# from script so it is usable now
+sed -i '/lxc-attach -n "$CTID" -- bash -c "$(curl -fsSL https:\/\/raw.githubusercontent.com\/community-scripts\/ProxmoxVE\/main\/install\/"$var_install".sh)" $?/d' "$tmp_dir/build.func"
+
+source <(cat "$tmp_dir/build.func")
+
+APP="Jellyfin"
+var_tags="media"
+var_cpu="10"
+var_ram="10240"
+var_disk="5"
+var_os="ubuntu"
+var_version="24.10"
+var_unprivileged="0"
+var_verbose="yes"
+var_fuse="no"
+var_tun="no"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -d /usr/lib/jellyfin ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  msg_info "Updating ${APP} LXC"
+  $STD apt-get update
+  $STD apt-get -y upgrade
+  $STD apt-get -y --with-new-pkgs upgrade jellyfin jellyfin-server
+  msg_ok "Updated ${APP} LXC"
+  exit
+}
+
+start
+build_container
+
+lxc-attach -n "$CTID" -- bash -c "$(curl -fsSL https://raw.githubusercontent.com/prizrak1609/proxmox-scripts/refs/heads/main/jellyfin/jellyfin-install.sh)"
+
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8096${CL}"
